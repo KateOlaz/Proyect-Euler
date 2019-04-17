2dfaa841a63e2360f9ec3410fdb324c82927d92d
KateOlaz <49486445+KateOlaz@users.noreply.github.com>
2019-04-17 11:26:58 -0500

    Update FactoresPrimos.cpp

--- FactoresPrimos.cpp
+++ FactoresPrimos.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main(){
-	int N;
+	long long N;
 	cout<<"N?: ";
 	cin>>N;
 	
 	long long mayor=0;
-	for(int j=2; j<=N;j++){
-		if(N%j==0){
+	for(long long j=2; j<=N;j++){
+		if(N%j==0 and j%2!=0){
 			int cont=0;
-			for(int k=2; k<=j;k++){
+			for(long long k=2; k<=j;k++){
 				if(j%k==0)
 					cont=cont+1;	
 			}
 			if(cont==1){
 				if(mayor<=j)
 					mayor=j;
+					cout<<mayor<<" ";	
 			}
 		}
-	
 	}
-cout<<mayor;	
+
 return 0;
 }
